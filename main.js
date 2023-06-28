271d4f4db3b8f52b0c9a64ce8e3fae5462d72023
DaniKef <gurenko888@gmail.com>
2023-06-28 19:16:34 +0300

    added ID

---
+++ main.js
@@ -0,0 +1,10 @@
+let tg = window.Telegram.WebApp; //получаем объект webapp телеграма
+
+var div = document.createElement("div");
+div.innerHTML =
+  '<div class="slideshow-container">\n' +
+  "<p>" +
+  tg.initDataUnsafe.user.id +
+  "</p>" +
+  "</div>\n";
+document.body.appendChild(div);
