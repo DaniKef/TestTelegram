310b6230b69b7b3f5560bb0d5b659af20e88e65a
DaniKef <gurenko888@gmail.com>
2023-06-29 00:22:42 +0300

    added main js

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-let tg = window.Telegram.WebApp; //получаем объект webapp телеграма
+// let tg = window.Telegram.WebApp; //получаем объект webapp телеграма
 
-var div = document.createElement("div");
-div.innerHTML =
-  '<div class="slideshow-container">\n' +
-  "<p>" +
-  tg.initDataUnsafe.user.id +
-  "</p>" +
-  "</div>\n";
-document.body.appendChild(div);
+// var div = document.createElement("div");
+// div.innerHTML =
+//   '<div class="slideshow-container">\n' + "<p>" + 1 + "</p>" + "</div>\n";
+// document.body.appendChild(div);
+window.onload = function () {
+  let tg = window.Telegram.WebApp; //получаем объект webapp телеграма
+
+  var div = document.createElement("div");
+  div.innerHTML =
+    '<div class="slideshow-container">\n' +
+    "<p>" +
+    tg?.initDataUnsafe?.user?.id +
+    "</p>" +
+    "</div>\n";
+  document.body.appendChild(div);
+};
